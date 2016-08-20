df8a4d055cb22bf337fcc9c9fb33f4e60482d04e
Alexander Trost <galexrt@googlemail.com>
2016-08-20 15:04:40 +0200

    Fixed a new line sign

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,6 @@ fi
 echo "Correcting mount point permissions ..."
 chown "$JTS3_USER":"$JTS3_GROUP" -R "$JTS3_DIR"
 
-echo "\nStarting JTS3ServerMod ..."
 cd "$JTS3_DIR" || exit 1
+echo "Starting JTS3ServerMod ..."
 exec sudo -u "$JTS3_USER" java "$JTS3_JAVA_ARGS" -jar "$JTS3_DIR/JTS3ServerMod.jar"
