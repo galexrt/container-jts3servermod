3e13dbcb047d69e71e0064388ba95ac39dd0f096
Stavros Kois <47820033+stavros-k@users.noreply.github.com>
2024-03-01 09:22:06 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ JTS3_LOG="${JTS3_LOG:-JTS3ServerMod_InstanceManager.cfg}"
 
 if [ ! "$(ls -A "$JTS3_DIR/config/")" ]; then
     echo "ENTRYPOINT: JTS3 Config volume is empty, copying default files to volume"
-    cp -ar "$JTS3_DIR/default_config/*" "$JTS3_DIR/config/"
+    cp -ar "$JTS3_DIR"/default_config/* "$JTS3_DIR/config/"
 fi
 
 cd "$JTS3_DIR" || { echo "ENTRYPOINT: Failed to enter JTS3_DIR ($JTS3_DIR), exiting"; exit 1; }
